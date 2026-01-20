0b269849974e316c6ce0fe0726efcfe748c6e89e
Luiz Gama <luizgamabh@gmail.com>
2026-01-20 02:16:49 -0300

    chore: fucking refactor

--- apps/pitanga-api/webpack.config.js
+++ apps/pitanga-api/webpack.config.js
@@ -1,6 +1,31 @@
-const { composePlugins, withNx } = require('@nx/webpack');
+const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { join } = require('path');
 
-module.exports = composePlugins(withNx(), (config) => {
-  // Additional customizations can be added here if needed
-  return config;
-});
+module.exports = {
+  output: {
+    path: join(__dirname, 'dist'),
+    clean: true,
+    ...(process.env.NODE_ENV !== 'production' && {
+      devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    }),
+  },
+  // externals: {
+  //   '@prisma/client': 'commonjs @prisma/client',
+  //   '@prisma/adapter-pg': 'commonjs @prisma/adapter-pg',
+  //   pg: 'commonjs pg',
+  //   'pg-native': 'commonjs pg-native',
+  // },
+  plugins: [
+    new NxAppWebpackPlugin({
+      target: 'node',
+      compiler: 'tsc',
+      main: './src/main.ts',
+      tsConfig: './tsconfig.app.json',
+      assets: ['./src/assets'],
+      optimization: false,
+      outputHashing: 'none',
+      generatePackageJson: false,
+      sourceMap: true,
+    }),
+  ],
+};
